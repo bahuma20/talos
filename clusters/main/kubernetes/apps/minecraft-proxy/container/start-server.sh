bc11addd9afbc66ee101bfe7155aff7e6d1aa45a
Max Bachhuber <max.bachhuber@bahuma.io>
2025-01-19 17:53:29 +0100

    Install minecraft-proxy

--- clusters/main/kubernetes/apps/minecraft-proxy/container/start-server.sh
+++ clusters/main/kubernetes/apps/minecraft-proxy/container/start-server.sh
@@ -12,21 +12,32 @@ TOKEN=$(cat ${SERVICEACCOUNT}/token)
 # Reference the internal certificate authority (CA)
 CACERT=${SERVICEACCOUNT}/ca.crt
 
+# Start server
+start() {
+  curl --cacert ${CACERT} \
+    --header "Authorization: Bearer ${TOKEN}" \
+    --header 'Content-Type: application/strategic-merge-patch+json' \
+    --request PATCH \
+    ${APISERVER}/apis/apps/v1/namespaces/"${APP_NAMESPACE}"/statefulsets/"${APP_STATEFULSET_NAME}" \
+    -d '{"spec":{"replicas":1}}' > /dev/null 2>&1
+}
 
-# Trap SIGTERM, forward it to server process ID
-trap 'kill -TERM $PID' TERM INT
+stop() {
+    curl --cacert ${CACERT} \
+      --header "Authorization: Bearer ${TOKEN}" \
+      --header 'Content-Type: application/strategic-merge-patch+json' \
+      --request PATCH \
+      ${APISERVER}/apis/apps/v1/namespaces/"${APP_NAMESPACE}"/statefulsets/"${APP_STATEFULSET_NAME}" \
+      -d '{"spec":{"replicas":0}}' > /dev/null 2>&1
+}
 
-# Start server (& at the end needed to keep it alive)
-curl --cacert ${CACERT} \
-  --header "Authorization: Bearer ${TOKEN}" \
-  --header 'Content-Type: application/strategic-merge-patch+json' \
-  --request PATCH \
-  ${APISERVER}/apis/apps/v1/namespaces/"${APP_NAMESPACE}"/statefulsets/"${APP_STATEFULSET_NAME}" \
-  -d '{"spec":{"replicas":1}}' &
+is_running() {
+      curl --cacert ${CACERT} \
+        --header "Authorization: Bearer ${TOKEN}" \
+        --header 'Content-Type: application/strategic-merge-patch+json' \
+        --request GET \
+        ${APISERVER}/apis/apps/v1/namespaces/"${APP_NAMESPACE}"/statefulsets/"${APP_STATEFULSET_NAME}" \
+        -d '{"spec":{"replicas":0}}' > /dev/null 2>&1
+}
 
-# Remember server process ID, wait for it to quit, then reset the trap
-PID=$!
-wait $PID
-trap - TERM INT
-wait $PID
 
