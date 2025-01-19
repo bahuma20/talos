24a84c48270865bcd0de553763679344813c7ca6
Max Bachhuber <max.bachhuber@bahuma.io>
2025-01-19 18:38:45 +0100

    Install minecraft-proxy

--- clusters/main/kubernetes/apps/minecraft-proxy/container/start-server.sh
+++ clusters/main/kubernetes/apps/minecraft-proxy/container/start-server.sh
@@ -12,8 +12,8 @@ TOKEN=$(cat ${SERVICEACCOUNT}/token)
 # Reference the internal certificate authority (CA)
 CACERT=${SERVICEACCOUNT}/ca.crt
 
-# Start server
-start() {
+start_server() {
+  echo "[APP] Starting server"
   curl --cacert ${CACERT} \
     --header "Authorization: Bearer ${TOKEN}" \
     --header 'Content-Type: application/strategic-merge-patch+json' \
@@ -22,22 +22,52 @@ start() {
     -d '{"spec":{"replicas":1}}' > /dev/null 2>&1
 }
 
-stop() {
-    curl --cacert ${CACERT} \
-      --header "Authorization: Bearer ${TOKEN}" \
-      --header 'Content-Type: application/strategic-merge-patch+json' \
-      --request PATCH \
-      ${APISERVER}/apis/apps/v1/namespaces/"${APP_NAMESPACE}"/statefulsets/"${APP_STATEFULSET_NAME}" \
-      -d '{"spec":{"replicas":0}}' > /dev/null 2>&1
+stop_server() {
+  echo "[APP] Stopping server"
+  curl --cacert ${CACERT} \
+    --header "Authorization: Bearer ${TOKEN}" \
+    --header 'Content-Type: application/strategic-merge-patch+json' \
+    --request PATCH \
+    ${APISERVER}/apis/apps/v1/namespaces/"${APP_NAMESPACE}"/statefulsets/"${APP_STATEFULSET_NAME}" \
+    -d '{"spec":{"replicas":0}}' > /dev/null 2>&1
 }
 
-is_running() {
-      curl --cacert ${CACERT} \
-        --header "Authorization: Bearer ${TOKEN}" \
-        --header 'Content-Type: application/strategic-merge-patch+json' \
-        --request GET \
-        ${APISERVER}/apis/apps/v1/namespaces/"${APP_NAMESPACE}"/statefulsets/"${APP_STATEFULSET_NAME}" \
-        -d '{"spec":{"replicas":0}}' > /dev/null 2>&1
+get_replicas_count() {
+  curl --cacert ${CACERT} \
+    --header "Authorization: Bearer ${TOKEN}" \
+    --request GET \
+    ${APISERVER}/apis/apps/v1/namespaces/"${APP_NAMESPACE}"/statefulsets/"${APP_STATEFULSET_NAME}" | jq ".status.replicas"
 }
 
 
+term() {
+  echo "[APP] Term received"
+  stop_server
+}
+
+
+trap term TERM INT
+
+start_server
+
+sleep 5
+
+replicas=1
+
+while [[ $replicas -ne 0 ]]; do
+  replicas=$(get_replicas_count)
+
+  echo "[APP] Aktuell $replicas replicas"
+
+  sleep 5
+done
+
+#trap - TERM INT
+
+while [[ $replicas -ne 0 ]]; do
+  replicas=$(get_replicas_count)
+
+  echo "[APP] Aktuell $replicas replicas (zweiter loop)"
+
+  sleep 5
+done
