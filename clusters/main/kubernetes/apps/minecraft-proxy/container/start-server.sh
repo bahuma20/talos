522a7aaf2f3cf06808a5f40f5b2b02ca77738a69
Max Bachhuber <max.bachhuber@bahuma.io>
2025-01-19 16:58:08 +0100

    Install minecraft-proxy

--- clusters/main/kubernetes/apps/minecraft-proxy/container/start-server.sh
+++ clusters/main/kubernetes/apps/minecraft-proxy/container/start-server.sh
@@ -12,10 +12,21 @@ TOKEN=$(cat ${SERVICEACCOUNT}/token)
 # Reference the internal certificate authority (CA)
 CACERT=${SERVICEACCOUNT}/ca.crt
 
-# Explore the API with TOKEN
+
+# Trap SIGTERM, forward it to server process ID
+trap 'kill -TERM $PID' TERM INT
+
+# Start server (& at the end needed to keep it alive)
 curl --cacert ${CACERT} \
   --header "Authorization: Bearer ${TOKEN}" \
   --header 'Content-Type: application/strategic-merge-patch+json' \
   --request PATCH \
   ${APISERVER}/apis/apps/v1/namespaces/"${APP_NAMESPACE}"/statefulsets/"${APP_STATEFULSET_NAME}" \
-  -d '{"spec":{"replicas":1}}'
+  -d '{"spec":{"replicas":1}}' &
+
+# Remember server process ID, wait for it to quit, then reset the trap
+PID=$!
+wait $PID
+trap - TERM INT
+wait $PID
+
